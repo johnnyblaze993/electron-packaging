b1aa45c1677e1da082fbc1817fa51e25c72f3f98
alvarezjo <jonathan.alvarez@axientcorp.com>
2024-11-12 08:49:47 -0600

    Remove build files from tracking

--- build/preload.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// electron/preload.ts
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld('electron', {
-// Expose any APIs you need here
-});
